df73bc9458914491b3ac1df280fc946b6ef2ebdf
Yamoon224 <Yamooon664@gmail.com>
2025-04-14 11:36:07 +0000

    modified:   routes/auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -15,7 +15,7 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
     Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create']) ->name('login');
+    Route::get('/', [AuthenticatedSessionController::class, 'create']) ->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot', [PasswordResetLinkController::class, 'create'])->name('password.request');
